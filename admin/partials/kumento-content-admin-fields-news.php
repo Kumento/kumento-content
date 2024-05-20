7d826253ff6381694c1d116258a48c8427a43f1a
Ulrik Vander <ulrik@vander-web.com>
2024-05-20 18:22:29 +0200

    main

--- admin/partials/kumento-content-admin-fields-news.php
+++ admin/partials/kumento-content-admin-fields-news.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The WPTurbo Custom Meta Box class.
+ * The kumento_news Custom Meta Box class.
  *
  * This class is responsible for rendering a custom meta box in WordPress with user-defined fields/options.
  * It uses the WordPress add_meta_box() and update_post_meta() functions to render and save field values.
@@ -33,12 +33,12 @@ class Kumento_News_Meta_Box {
 	 * @var array $fields The list of user defined fields/options.
 	 */
 	private $fields = [
-		'kumento-content-publicist' => [
+		'kumento_content_publicist' => [
 			'type' => 'text',
 			'label' => 'Publicist',
 			'default' => '',
 		],
-		'kumento-content-country' => [
+		'kumento_content_country' => [
 			'type' => 'select',
 			'label' => 'Country',
 			'default' => 'Danmark',
@@ -46,22 +46,22 @@ class Kumento_News_Meta_Box {
 				'Danmark',
 			],
 		],
-		'kumento-content-contact-phoneno' => [
+		'kumento_content_contact_phoneno' => [
 			'type' => 'number',
 			'label' => 'Contact Phoneno',
 			'default' => '',
 		],
-		'kumento-content-contact-email' => [
+		'kumento_content_contact_email' => [
 			'type' => 'email',
 			'label' => 'Email',
 			'default' => '',
 		],
-		'kumento-content-homepage-link' => [
+		'kumento_content_homepage_link' => [
 			'type' => 'url',
 			'label' => 'Link to Homepage',
 			'default' => '',
 		],
-		'kumento-content-youtube-link' => [
+		'kumento_content_youtube_link' => [
 			'type' => 'url',
 			'label' => 'Youtube Link',
 			'default' => '',
@@ -76,8 +76,9 @@ class Kumento_News_Meta_Box {
 	 * @since 1.0.0
 	 */
 	public function __construct() {
-		add_action( 'add_meta_boxes', [ $this, 'wpturbo_register_meta_boxes' ] );
-		add_action( 'save_post', [ $this, 'wpturbo_save_meta_box_fields' ] );
+		add_action( 'add_meta_boxes', [ $this, 'kumento_news_register_meta_boxes' ] );
+		add_action( 'save_post', [ $this, 'kumento_news_save_meta_box_fields' ] );
+		add_action('rest_api_init', [ $this, 'kumento_register_news_post_meta_fields' ] );
 	}
 	
 	/**
@@ -88,12 +89,12 @@ class Kumento_News_Meta_Box {
 	 *
 	 * @return void
 	 */
-	public function wpturbo_register_meta_boxes() : void {
+	public function kumento_news_register_meta_boxes() : void {
 		foreach ( $this->display_locations as $location ) {
 			add_meta_box(
 				'kumento-content-news-meta-box', /* The id of our meta box. */
 				__( 'Fields', 'kumento_content' ), /* The title of our meta box. */
-				[ $this, 'wpturbo_render_meta_box_fields' ], /* The callback function that renders the metabox. */
+				[ $this, 'kumento_news_render_meta_box_fields' ], /* The callback function that renders the metabox. */
 				$location, /* The screen on which to show the box. */
 				'normal', /* The placement of our meta box. */
 				'high', /* The priority of our meta box. */
@@ -111,7 +112,7 @@ class Kumento_News_Meta_Box {
 	 *
 	 * @return void
 	 */
-	public function wpturbo_render_meta_box_fields(WP_Post $post) : void {
+	public function kumento_news_render_meta_box_fields(WP_Post $post) : void {
 		wp_nonce_field( 'kumento-content-news-meta-box_data', 'kumento-content-news-meta-box_nonce' );
 		$html = '';
 		foreach( $this->fields as $field_id => $field ){
@@ -120,9 +121,9 @@ class Kumento_News_Meta_Box {
 				$meta_value = $field['default'];
 			}
 	
-			$field_html = $this->wpturbo_render_input_field( $field_id, $field, $meta_value );
+			$field_html = $this->kumento_news_render_input_field( $field_id, $field, $meta_value );
 			$label = "<label for='$field_id'>{$field['label']}</label>";
-			$html .= $this->wpturbo_format_field( $label, $field_html );
+			$html .= $this->kumento_news_format_field( $label, $field_html );
 		}
 		echo '<table class="form-table"><tbody>' . $html . '</tbody></table>';
 	}
@@ -138,7 +139,7 @@ class Kumento_News_Meta_Box {
 	 *
 	 * @return string
 	 */
-	public function wpturbo_format_field( string $label, string $field ): string {
+	public function kumento_news_format_field( string $label, string $field ): string {
 		return '<tr class="form-field"><th>' . __( $label, 'kumento_content' ) . '</th><td>' . $field . '</td></tr>';
 	}
 	
@@ -154,7 +155,7 @@ class Kumento_News_Meta_Box {
 	 *
 	 * @return string The HTML code.
 	 */
-	public function wpturbo_render_input_field( string $field_id, array $field, string $field_value): string {
+	public function kumento_news_render_input_field( string $field_id, array $field, string $field_value): string {
 		switch( $field['type'] ){
 			case 'select': {
 				$field_html = '<select name="'.$field_id.'" id="'.$field_id.'">';
@@ -194,7 +195,7 @@ class Kumento_News_Meta_Box {
 	 *
 	 * @return int The post ID.
 	 */
-	public function wpturbo_save_meta_box_fields( int $post_id ) {
+	public function kumento_news_save_meta_box_fields( int $post_id ) {
 		if ( ! isset( $_POST['kumento-content-news-meta-box_nonce'] ) ) return;
 	
 		$nonce = $_POST['kumento-content-news-meta-box_nonce'];
@@ -220,6 +221,75 @@ class Kumento_News_Meta_Box {
 		}
 	}
 	
+	public function kumento_register_news_post_meta_fields() {
+
+		foreach ( $this->display_locations as $location ) {
+			register_rest_field(
+				$location, // Object post|term|comment etc.
+				'kumento_content_publicist', // Name of field
+				array(
+					'get_callback'    => [ $this, 'kumento_get_all_post_meta' ],
+					'update_callback'    => [ $this, 'kumento_update_post_meta' ],
+					'schema'          => null, // Schema for the field
+				)
+			);
+			register_rest_field(
+				$location, // Object post|term|comment etc.
+				'kumento_content_country', // Name of field
+				array(
+					'get_callback'    => [ $this, 'kumento_get_all_post_meta' ],
+					'update_callback'    => [ $this, 'kumento_update_post_meta' ],
+					'schema'          => null, // Schema for the field
+				)
+			);
+			register_rest_field(
+				$location, // Object post|term|comment etc.
+				'kumento_content_contact_phoneno', // Name of field
+				array(
+					'get_callback'    => [ $this, 'kumento_get_all_post_meta' ],
+					'update_callback'    => [ $this, 'kumento_update_post_meta' ],
+					'schema'          => null, // Schema for the field
+				)
+			);
+			register_rest_field(
+				$location, // Object post|term|comment etc.
+				'kumento_content_contact_email', // Name of field
+				array(
+					'get_callback'    => [ $this, 'kumento_get_all_post_meta' ],
+					'update_callback'    => [ $this, 'kumento_update_post_meta' ],
+					'schema'          => null, // Schema for the field
+				)
+			);
+			register_rest_field(
+				$location, // Object post|term|comment etc.
+				'kumento_content_homepage_link', // Name of field
+				array(
+					'get_callback'    => [ $this, 'kumento_get_all_post_meta' ],
+					'update_callback'    => [ $this, 'kumento_update_post_meta' ],
+					'schema'          => null, // Schema for the field
+				)
+			);
+			register_rest_field(
+				$location, // Object post|term|comment etc.
+				'kumento_content_youtube_link', // Name of field
+				array(
+					'get_callback'    => [ $this, 'kumento_get_all_post_meta' ],
+					'update_callback'    => [ $this, 'kumento_update_post_meta' ],
+					'schema'          => null, // Schema for the field
+				)
+			);
+		}
+		}
+		
+	
+	/* Callback function */
+	public function kumento_get_all_post_meta($object, $field_name, $request) {
+		return get_post_meta($object['id'], $field_name, true);
+	}
+	
+	public function kumento_update_post_meta($value, $object, $field_name) {
+		return update_post_meta($object['id'], $field_name, $value);
+	}
 }
 
 if ( class_exists( 'Kumento_News_Meta_Box' ) ) {
